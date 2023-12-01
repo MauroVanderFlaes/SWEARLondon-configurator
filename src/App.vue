cce474e3e968c77af68bcd75f9bb7ce89f9b7b9f
TiboMertens <tibomertens25@gmail.com>
2023-12-01 23:30:38 +0100

    feat: delete sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <script setup>
 import Configurator from './components/Configurator.vue';
 import Modifier from './components/Modifier.vue';
-import Sidebar from './components/Sidebar.vue';
-
 </script>
 
 <template>
-  <Sidebar />
   <Configurator />
   <Modifier />
 </template>
