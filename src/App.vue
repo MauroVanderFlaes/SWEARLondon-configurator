146e35d2b290e6e65120c80ed7a5d1202580ee4a
TiboMertens <tibomertens25@gmail.com>
2023-12-01 21:48:57 +0100

    feat: add color options

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Configurator from './components/Configurator.vue';
+import Modifier from './components/Modifier.vue';
 
 </script>
 
 <template>
-  <div>
-    <Configurator />
-  </div>
+  <Configurator class="" />
+  <Modifier class="" />
 </template>
+
+<style scoped></style>
---
+++ src/components/Modifier.vue
@@ -0,0 +1,51 @@
+<script setup>
+</script>
+
+<template>
+    <div class="fixed bottom-0 left-0 right-0 bg-stone-200 py-7">
+        <h1 class="text-center text-[2em] font-bold pb-7">Color modifier</h1>
+        <div class="flex justify-center gap-8 items-center flex-wrap">
+            <div class="text-center">
+                <!-- Make a circle using tailwindcss -->
+                <div class="w-[42px] h-[42px] rounded-full bg-[#FF0000] mx-auto"></div>
+                <h2 class="mt-2">Red</h2>                
+            </div>
+            <div class="text-center">
+                <!-- Make a circle using tailwindcss -->
+                <div class="w-[42px] h-[42px] rounded-full bg-[#008000] mx-auto"></div>
+                <h2 class="mt-2">Green</h2>
+            </div>
+            <div class="text-center">
+                <!-- Make a circle using tailwindcss -->
+                <div class="w-[42px] h-[42px] rounded-full bg-[#FFC0CB] mx-auto"></div>
+                <h2 class="mt-2">Pink</h2>
+            </div>
+            <div class="text-center">
+                <!-- Make a circle using tailwindcss -->
+                <div class="w-[42px] h-[42px] rounded-full bg-[#00FFFF] mx-auto"></div>
+                <h2 class="mt-2">Cyan</h2>
+            </div>
+            <div class="text-center">
+                <!-- Make a circle using tailwindcss -->
+                <div class="w-[42px] h-[42px] rounded-full bg-[#87CEEB] mx-auto"></div>
+                <h2 class="mt-2">Sky</h2>
+            </div>
+            <div class="text-center">
+                <!-- Make a circle using tailwindcss -->
+                <div class="w-[42px] h-[42px] rounded-full bg-[#A020F0] mx-auto"></div>
+                <h2 class="mt-2">Purple</h2>
+            </div>
+            <div class="text-center">
+                <!-- Make a circle using tailwindcss -->
+                <div class="w-[42px] h-[42px] rounded-full bg-[#000000] mx-auto"></div>
+                <h2 class="mt-2">Black</h2>
+            </div>
+            <div class="text-center">
+                <!-- Make a circle using tailwindcss -->
+                <div class="w-[42px] h-[42px] rounded-full bg-[#ffffff] mx-auto"></div>
+                <h2 class="mt-2">White</h2>
+            </div>
+        </div>
+    </div>
+</template>
+  
